e7538d1a4ead4ff1747e1f83ddad15ebfd81ea96
Abhishek Kwatra <95302238+abhishek-kwatra@users.noreply.github.com>
2025-08-22 01:30:33 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from backend.routes.upload import router as upload_router
 from backend.routes.query import router as query_router
 from backend.routes.metadata import router as metadata_router
-from backend.config.config import get_connection
 from backend.services.weaviate_client import client
 
 app = FastAPI(title="RAG API")
@@ -10,32 +9,6 @@ app = FastAPI(title="RAG API")
 app.include_router(upload_router)
 app.include_router(query_router)
 app.include_router(metadata_router)
-
-create_tables_sql = """
-CREATE TABLE IF NOT EXISTS documents (
-    id SERIAL PRIMARY KEY,
-    filename TEXT UNIQUE NOT NULL,
-    pages INT,
-    uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS chunks (
-    id SERIAL PRIMARY KEY,
-    doc_id INT REFERENCES documents(id) ON DELETE CASCADE,
-    text TEXT,
-    vector_id TEXT
-);
-"""
-
-@app.on_event("startup")
-def init_db():
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(create_tables_sql)
-    conn.commit()
-    cur.close()
-    conn.close()
-    print("✅ Tables ensured at startup")
 
 @app.get("/")
 def root():
